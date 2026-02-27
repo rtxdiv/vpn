f9d47e7619d3830c71679d72904b3371c4875d72
rtxdiv <mitrushonkovdima@gmail.com>
2026-02-27 17:01:00 +0300

    upd

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, send_file
+from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../public', static_url_path='')
 
 @app.get('/')
 def getRoot():
-    return send_file('../static/index.html')
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8443)
