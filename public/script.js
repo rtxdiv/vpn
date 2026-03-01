e13e3389e3581671f036bf1188283912900cc5d2
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-01 22:40:56 +0300

    upd

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const debug = document.querySelector('#debug')
 const telegram = window.Telegram.WebApp
 telegram.expand()
 
+// {"email":"5019916197","enable":true,"id":"3657bbfc-6f79-454b-a3e5-87442c5df7a5","password":"","inboundId":null,"up":0,"down":0,"expiryTime":1774461052796,"total":0,"reset":0,"flow":"","method":"","limitIp":3,"subId":"3657bbfc-6f79-454b-a3e5-87442c5df7a5","comment":"","tgId":"","totalGB":0,"uuid":null}
 const main = async () => {
     const resp = await fetch('/user', {
         headers: {
@@ -10,9 +11,7 @@ const main = async () => {
             'Authorization': `Telegram ${telegram.initData}`
         }
     })
-    debug.innerHTML += `${JSON.stringify(resp)}`
     const body = await resp.json()
-    debug.innerHTML += `\n${JSON.stringify(body)}`
 }
 
 main()
