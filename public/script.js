9b8521d4882a0b08ce87ad5e4fdaba050f00c5ba
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-02 01:17:37 +0300

    upd

--- public/script.js
+++ public/script.js
@@ -46,9 +46,8 @@ function drawSub(user, error = false) {
         subDate.innerHTML = `Действует до: ${ localDate }`
 
         subConf.addEventListener('click', function(event) {
-            event.target.style.color = 'red'
-            telegram.openLink('https://google.com')
-            // location.hostname + ':8080/sub/' + user['subId']
+            event.target.innerHTML = location.hostname
+            telegram.openLink('https://' + location.hostname + ':8080/sub/' + user['subId'])
         })
         
     } else {
