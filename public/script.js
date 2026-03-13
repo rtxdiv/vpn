bcaa0bc976f08a21a5a0c34219ace3e5d88c90e4
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-13 14:23:44 +0300

    upd

--- public/script.js
+++ public/script.js
@@ -132,16 +132,15 @@ function displayTariffs({ tariffs = false, error = false }) {
     }
 }
 function displaySettings({ settings = false, error = false }) {
-    console.log(settings.message)
-    console.log(error)
+    console.log(settings)
     if (error) {
         settingsMessage.querySelector('.message').innerHTML = 'Ошибка при загрузке настроек'
         settingsMessage.classList.add('error-block')
         settingsMessage.display = 'flex'
         return
     }
-    if (settings.message) {
-        settingsMessage.querySelector('.message').innerHTML = settings.message
+    if (settings['message']) {
+        settingsMessage.querySelector('.message').innerHTML = settings['message']
         settingsMessage.display = 'flex'
     }
 }
