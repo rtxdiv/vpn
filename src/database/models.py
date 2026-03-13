c4b7c51d271e722f20ce49ffce3812d9144551cc
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-13 15:49:19 +0300

    upd

--- src/database/models.py
+++ src/database/models.py
@@ -32,6 +32,6 @@ class Settings(Base):
     __tablename__ = 'settings'
 
     message: Mapped[str] = mapped_column(String)
-    about_link: Mapped[str] = mapped_column(String)
-    tutorial_link: Mapped[str] = mapped_column(String)
-    support_link: Mapped[str] = mapped_column(String)
+    about_url: Mapped[str] = mapped_column(String)
+    tutorial_url: Mapped[str] = mapped_column(String)
+    support_url: Mapped[str] = mapped_column(String)
