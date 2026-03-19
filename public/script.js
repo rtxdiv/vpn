59f821a250400048f1c68d89f5565414d00b8660
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-19 13:55:43 +0300

    upd

--- public/script.js
+++ public/script.js
@@ -222,7 +222,7 @@ function closePopup(event) {
         popup.classList.remove('opened')
         setTimeout(() => {
             popupBg.style.display = 'none'
-        }, 200)
+        }, 300)
         document.body.style.overflow = 'auto'
         popupIsOpened = false
     }
