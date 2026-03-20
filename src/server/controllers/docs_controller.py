4e27b6e23bd6a5186abda25f4e7d01cef9e4f5b1
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-20 20:18:15 +0300

    upd

--- src/server/controllers/docs_controller.py
+++ src/server/controllers/docs_controller.py
@@ -11,4 +11,4 @@ async def get_docs(file: str):
     path = PUBLIC_DIR / 'docs' / f'{file}.html'
     if os.path.exists(path):
         return FileResponse(path)
-    raise HTTPException(status_code=404, detail='File not found')
+    raise HTTPException(status_code=404)
