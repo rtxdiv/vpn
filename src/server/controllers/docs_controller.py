e04547438c75ff5ccd072d36d5f34f7731c0faea
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-20 19:57:37 +0300

    upd

--- src/server/controllers/docs_controller.py
+++ src/server/controllers/docs_controller.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
+from fastapi.responses import FileResponse
 from root import PUBLIC_DIR
 
 
 docs_router = APIRouter(prefix='/docs')
 
-@docs_router.get('/{document}')
-async def get_docs(document: str):
-    print(document)
+@docs_router.get('/{file}')
+async def get_docs(file: str):
+    return FileResponse(PUBLIC_DIR / 'docs' / file + '.html')
