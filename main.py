d1d4aae35560daa4209b5668ed56abeb67e66bae
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-25 14:16:47 +0300

    upd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 from src.server.fastapi_server import app
 from src.xui.xui_client import xui
 from src.bot.bot_server import bot, dp
+from src.bot.bot_commands import commands_router
 
 
 async def main():
@@ -12,6 +13,7 @@ async def main():
     server = uvicorn.Server(config)
     await xui.login()
     await bot.delete_webhook(drop_pending_updates=True)
+    dp.include_router(commands_router)
 
     await asyncio.gather(
         server.serve(),
