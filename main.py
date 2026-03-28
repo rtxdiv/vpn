c450638f48be1884536f9b13494270fd8461be4a
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-28 05:45:56 +0300

    upd

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import uvicorn
 from src.server.fastapi_server import app
 from src.xui.xui_client import xui
 from src.bot.bot_server import bot, dp, register_routers
+from src.http.http_client import init_http_session, close_http_session
 
 
 async def main():
@@ -14,11 +15,15 @@ async def main():
     await bot.delete_webhook(drop_pending_updates=True)
     register_routers()
 
-    await asyncio.gather(
-        server.serve(),
-        asyncio.create_task(dp.start_polling(bot)),
-        return_exceptions=True
-    )
+    await init_http_session()
+    try:
+        await asyncio.gather(
+            server.serve(),
+            asyncio.create_task(dp.start_polling(bot)),
+            return_exceptions=True
+        )
+    finally:
+        await close_http_session()
 
 if __name__ == '__main__':
     asyncio.run(main())
