198dfea47bea5e9f9bfa6ca1276fc13359b08c40
rtxdiv <mitrushonkovdima@gmail.com>
2026-03-28 07:04:34 +0300

    upd

--- src/server/dto/payment_buy_dto.py
+++ src/server/dto/payment_buy_dto.py
@@ -2,6 +2,23 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class BuyDto(BaseModel):
-    uname: str = Field(..., description='uname обязательное поле')
+    type: str = 'Buy'
+    to_tariff_uname: str = Field(..., min_length=1)
     months: Optional[int] = Field(None)
     for_pay: bool = Field(default=False)
+
+class UpgradeDto(BaseModel):
+    type: str = 'Upgrade'
+    user_period_id: Optional[int] = Field(None)
+    from_tariff_uname: str = Field(..., min_length=1)
+    to_tariff_uname: str = Field(..., min_length=1)
+    for_pay: bool = Field(default=False)
+
+
+# ПУТЬ ЗАПРОСА НА ПОКУПКУ И АПГРЕЙД:
+# FastAPI сервер (DTO) -> 
+# -> DB service:
+#      1. Готовит данные для платежа (order, user, сумма)
+#      2. Просит создать платёж
+#      3. Создаёт запись в payments (вместе с нужными данными)
+#      4. Возвращает данные для оплаты
