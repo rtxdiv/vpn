7cfc114961cd9c874d0d66c7cd07ccb9917a7373
rtxdiv <mitrushonkovdima@gmail.com>
2026-04-03 06:58:00 +0300

    upd

--- src/xui/xui_service.py
+++ src/xui/xui_service.py
@@ -86,7 +86,6 @@ class XUIClient:
     
 
     async def create_client(self, user_id: str, limit_ip: int, expiry: int, comment: str) -> py3xui.Client:
-        print('CREATE', flush=True)
         if not user_id: raise GetTgIdException
         uuid4 = await self.get_new_uuid()
         new_client = py3xui.Client(
@@ -107,7 +106,7 @@ class XUIClient:
     
 
     async def update_client(self, uuid4: str, new_client: py3xui.Client):
-        print(new_client.flow, flush=True)
+        error_log.error(new_client.flow)
         try: await self._api.client.update(uuid4, new_client)
         except Exception as exc:
             error_log.error(exc)
