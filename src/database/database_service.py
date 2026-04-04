a60e8ac01ef09abe01b8fa2fd82393da91cb5b03
rtxdiv <mitrushonkovdima@gmail.com>
2026-04-04 18:29:31 +0300

    upd

--- src/database/database_service.py
+++ src/database/database_service.py
@@ -95,7 +95,7 @@ async def prepare_buy(session: AsyncSession, user_id: str, uname: str, months: i
     if (last_period):
         starts = last_period.starts + timedelta(days=last_period.days)
     else:
-        datetime.now(timezone.utc).date()
+        starts = datetime.now(timezone.utc).date()
 
     total = round(tariff.price * period.months * (1 - period.discount))
 
