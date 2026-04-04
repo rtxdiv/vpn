4bcb07117ed4c4e1ca6148e2c185ece53abf8153
rtxdiv <mitrushonkovdima@gmail.com>
2026-04-05 00:38:08 +0300

    upd

--- public/index.js
+++ public/index.js
@@ -296,7 +296,6 @@ function displayBuy({ info = false, error = false }) {
         popupTitle.innerHTML = info.title
         const date = new Date(info.starts+'Z')
         const currDate = new Date()
-        alert(`${date.setHours(0,0,0,0)}\n${currDate.setHours(0,0,0,0)}`)
         const localDate = date.setHours(0,0,0,0) == currDate.setHours(0,0,0,0)
             ? 'сегодня'
             : date.toLocaleDateString('ru-RU', {
--- src/database/database_service.py
+++ src/database/database_service.py
@@ -97,7 +97,6 @@ async def prepare_buy(session: AsyncSession, user_id: str, uname: str, months: i
         starts = last_period.starts + timedelta(days=last_period.days)
     else:
         starts = datetime.now(timezone.utc).date()
-    print(starts, flush=True)
 
     total = round(tariff.price * period.months * (1 - period.discount))
 
