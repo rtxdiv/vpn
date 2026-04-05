24d7cda0d6d9f02911dfa231aa3c3b8c330f44c8
rtxdiv <mitrushonkovdima@gmail.com>
2026-04-05 06:42:10 +0300

    upd

--- public/payments.js
+++ public/payments.js
@@ -91,7 +91,7 @@ function displayPayments({ payments = false, error = false }) {
     if (payments && payments.length != 0) {
         payments.forEach(payment => {
             payment.success? successPayments.push(payment.payment_id) : null
-            const date = new Date(payment.created)
+            const date = new Date(payment.created+'Z')
             const localDate = date.toLocaleDateString('ru-RU', {
                 day: 'numeric',
                 month: 'short',
