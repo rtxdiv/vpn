60e381b5cd351b1cea3e23d17731ba148e4fe967
rtxdiv <mitrushonkovdima@gmail.com>
2026-04-05 08:06:25 +0300

    upd

--- public/index.js
+++ public/index.js
@@ -298,8 +298,7 @@ function displayBuy({ info = false, error = false }) {
         popupTitle.innerHTML = info.title
         const date = new Date(info.starts)
         const currDate = new Date()
-        alert(date)
-        const localDate = date.setHours(0,0,0,0) == currDate(0,0,0,0)
+        const localDate = date.setHours(0,0,0,0) == currDate.setHours(0,0,0,0)
             ? 'сегодня'
             : date.toLocaleDateString('ru-RU', {
                 day: 'numeric',
