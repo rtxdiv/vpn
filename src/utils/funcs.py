fbc4be80ac65bf2f6a4d25d27e0f78f1cf511563
rtxdiv <mitrushonkovdima@gmail.com>
2026-06-15 23:35:50 +0300

    update

--- src/utils/funcs.py
+++ src/utils/funcs.py
@@ -4,10 +4,13 @@ from datetime import datetime
 def bytes_to_gb(bytes):
     return round(bytes / (1024 * 1024 * 1024), 1)
 
-def format_date(timestamp: str) -> str:
+def format_date(date_input):
+    if isinstance(date_input, datetime):
+        dt = date_input
+    elif isinstance(date_input, str):
+        dt = datetime.fromisoformat(date_input)
+    else:
+        return str(date_input)
+    
     months = ["января", "февраля", "марта", "апреля", "мая", "июня", "июля", "августа", "сентября", "октября", "ноября", "декабря"]
-    dt = datetime.fromisoformat(timestamp)
-    day = dt.day
-    month_name = months[dt.month - 1]
-    year = dt.year
-    return f'{day} {month_name} {year}'
+    return f'{dt.day} {months[dt.month - 1]} {dt.year}'
